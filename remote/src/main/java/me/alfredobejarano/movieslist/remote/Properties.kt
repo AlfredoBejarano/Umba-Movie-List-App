4c8f12ab6caaebd223567000842233feed0a570a
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-01 17:38:28 -0500

    Create Properties.kt
    object class that fetches properties from local.properties.

---
+++ remote/src/main/java/me/alfredobejarano/movieslist/remote/Properties.kt
@@ -0,0 +1,10 @@
+package me.alfredobejarano.movieslist.remote
+
+/**
+ * Created by alfredo on 2019-08-01.
+ * Copyright © 2019 GROW. All rights reserved.
+ */
+object Properties {
+    val THE_MOVIE_DB_API_KEY = System.getProperty("theMovieDBApiKey")
+    val THE_MOVIE_DB_API_BASE_URL = System.getProperty("theMovieDBBaseURL")
+}
