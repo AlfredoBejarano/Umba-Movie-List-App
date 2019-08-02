8dde6a446d89d1cf513c3ea3dc6d34515348e504
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-02 12:10:42 -0500

    Create MovieListType enum class

---
+++ core/src/main/java/me/alfredobejarano/movieslist/core/MovieListType.kt
@@ -0,0 +1,11 @@
+package me.alfredobejarano.movieslist.core
+
+/**
+ * Created by alfredo on 2019-08-02.
+ * Copyright © 2019 GROW. All rights reserved.
+ */
+enum class MovieListType {
+    MOVIE_LIST_POPULAR,
+    MOVIE_LIST_UPCOMING,
+    MOVIE_LIST_TOP_RATED
+}
