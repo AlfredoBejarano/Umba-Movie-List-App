57f8c5359bd9b83bef80b5cb719e6648a0bf7b3e
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-02 10:53:09 -0500

    Create MovieResult.kt

--- remote/src/main/java/me/alfredobejarano/movieslist/remote/Properties.kt
+++ remote/src/main/java/me/alfredobejarano/movieslist/remote/Properties.kt
@@ -2,7 +2,6 @@ package me.alfredobejarano.movieslist.remote
 
 /**
  * Created by alfredo on 2019-08-01.
- * Copyright © 2019 GROW. All rights reserved.
  */
 object Properties {
     val THE_MOVIE_DB_API_KEY = System.getProperty("theMovieDBApiKey")
