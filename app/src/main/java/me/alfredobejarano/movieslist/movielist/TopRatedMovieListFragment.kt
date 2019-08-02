4ac5055d241f5e332863a5a73f94a84cee8c9955
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-02 17:12:55 -0500

    Create implementations of MovieListFragment
    Add implementations for:
    Popular Movies
    Top Rated Movies
    Upcoming Movies

---
+++ app/src/main/java/me/alfredobejarano/movieslist/movielist/TopRatedMovieListFragment.kt
@@ -0,0 +1,12 @@
+package me.alfredobejarano.movieslist.movielist
+
+import me.alfredobejarano.movieslist.core.MovieListType
+import me.alfredobejarano.movieslist.movielist.base.MovieListFragment
+
+/**
+ * Created by alfredo on 2019-08-02.
+ * Copyright © 2019 GROW. All rights reserved.
+ */
+class TopRatedMovieListFragment : MovieListFragment() {
+    override val listType = MovieListType.MOVIE_LIST_TOP_RATED
+}
