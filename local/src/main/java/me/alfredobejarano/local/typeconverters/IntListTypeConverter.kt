dcbfeb60c15909febe3d13fb4b41207707079f05
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-02 12:50:19 -0500

    Create IntListTypeConverter.kt

---
+++ local/src/main/java/me/alfredobejarano/local/typeconverters/IntListTypeConverter.kt
@@ -0,0 +1,27 @@
+package me.alfredobejarano.local.typeconverters
+
+import androidx.room.TypeConverter
+
+/**
+ * Created by alfredo on 2019-08-02.
+ */
+class IntListTypeConverter {
+    companion object {
+        @TypeConverter()
+        fun fromListToString(list: List<Int>) = StringBuilder().apply {
+            when (list.size) {
+                0 -> append("")
+                1 -> append(list.first().toString())
+                else -> {
+                    for (i in 0 until list.size - 1) {
+                        append("${list[i]},")
+                    }
+                    append(list.last().toString())
+                }
+            }
+        }.toString()
+
+        @TypeConverter
+        fun fromStringToList(movies: String) = movies.split(",")
+    }
+}
