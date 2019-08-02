d7cbb6274c161b36d4e7a2ed7582e87432753dd1
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-02 16:40:22 -0500

    Create MovieListViewModel.kt

---
+++ app/src/main/java/me/alfredobejarano/movieslist/movielist/MovieListViewModel.kt
@@ -0,0 +1,15 @@
+package me.alfredobejarano.movieslist.movielist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.launchInIOForLiveData
+import me.alfredobejarano.movieslist.core.MovieListType
+import me.alfredobejarano.movieslist.domain.GetMovieListUseCase
+
+/**
+ * Created by alfredo on 2019-08-02.
+ */
+class MovieListViewModel(private val getListUseCase: GetMovieListUseCase) : ViewModel() {
+    fun getMovieList(type: MovieListType) = launchInIOForLiveData {
+        getListUseCase.getMovieList(type)
+    }
+}
