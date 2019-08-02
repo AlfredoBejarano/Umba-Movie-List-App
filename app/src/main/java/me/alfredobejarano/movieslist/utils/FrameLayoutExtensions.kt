a43d1e77a293ec3f0aa1db6193743bb45730bf73
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-02 17:07:54 -0500

    Create FrameLayoutExtensions.kt

---
+++ app/src/main/java/me/alfredobejarano/movieslist/utils/FrameLayoutExtensions.kt
@@ -0,0 +1,15 @@
+package me.alfredobejarano.movieslist.utils
+
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+/**
+ * Created by alfredo on 2019-08-02.
+ */
+
+fun FrameLayout.replaceFragment(fragmentManager: FragmentManager, fragment: Fragment) =
+    fragmentManager.beginTransaction()
+        .replace(id, fragment)
+        .disallowAddToBackStack()
+        .commitAllowingStateLoss()
