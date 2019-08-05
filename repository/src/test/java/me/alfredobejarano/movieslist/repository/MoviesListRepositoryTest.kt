6e9480cae4d080dc60b1e021ddc0e742d644e66a
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-05 17:56:26 -0500

    Create MovieListRepository.kt

---
+++ repository/src/test/java/me/alfredobejarano/movieslist/repository/MoviesListRepositoryTest.kt
@@ -0,0 +1,21 @@
+package me.alfredobejarano.movieslist.repository
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MoviesListRepositoryTest {
+
+    @Test
+    fun getMoviesListBy() {
+    }
+
+    @Test
+    fun findMovieByTitle() {
+    }
+
+    @Test
+    fun getMovieListResult() {
+    }
+}
