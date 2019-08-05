7c53539239bb0318b76870bd44ccef40de3731f2
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-05 01:09:25 -0500

    Create MovieDetailsDao.kt

--- core/src/main/java/me/alfredobejarano/movieslist/core/MovieDetails.kt
+++ core/src/main/java/me/alfredobejarano/movieslist/core/MovieDetails.kt
@@ -1,10 +1,12 @@
 package me.alfredobejarano.movieslist.core
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "movie_details")
 data class MovieDetails(
+    @ColumnInfo(name = "pk")
     @PrimaryKey(autoGenerate = false)
     val id: Int,
     val title: String,
