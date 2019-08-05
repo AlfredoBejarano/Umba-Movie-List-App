81ff8ee3f45906d18843fab56813d5cf550cd32c
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-05 00:44:04 -0500

    Create MovieDetail.kt
    Entity class to cache the details of a movie

---
+++ core/src/main/java/me/alfredobejarano/movieslist/core/MovieDetails.kt
@@ -0,0 +1,17 @@
+package me.alfredobejarano.movieslist.core
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movie_details")
+data class MovieDetails(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val title: String,
+    val runtime: Int,
+    val votesCount: Int,
+    val genres: List<String>,
+    val posterURL: String,
+    val overview: String,
+    val videoKey: String
+)
